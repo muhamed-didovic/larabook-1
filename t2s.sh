8de4012253ea32e66816c2863e6802b1e4e7c3a1
Ian Simmons <isimmons33@gmail.com>
2014-07-16 18:34:53 -0400

    think I fixed an issue in my shell script

--- t2s.sh
+++ t2s.sh
@@ -2,5 +2,6 @@
 
 #ignore me unless you want a command to convert all your tabs to spaces excluding the vendor directory
 echo "converting tabs to spaces"
-find . -path ./vendor -prune -o -name '*.php' ! -type d -exec bash -c 'expand -t 4 "$0" > /tmp/e && mv /tmp/e "$0"' {} \;
+find . -name '*.php' ! -type d -path './vendor' -prune -o  -path './node_modules' -prune -exec bash -c 'expand -t 4 "$0" > /tmp/e && mv /tmp/e "$0"' {} \;
+#find . -prune \( -name vendor -o -name node_modules \) ! -type d -exec bash -c 'expand -t 4 "$0" > /tmp/e && mv /tmp/e "$0"' {} \;
 echo "Finished!"
