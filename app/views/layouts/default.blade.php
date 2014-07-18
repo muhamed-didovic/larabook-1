ddec8445354d6b3d5c8cb09c19053f300b44fc37
Ian Simmons <isimmons33@gmail.com>
2014-07-17 21:21:01 -0400

    change css relative link to HTML::style()

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -6,7 +6,7 @@
 
     <!-- Bootstrap CSS CDN -->
     <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/main.css">
+    {{{ HTML::style('css/main.css') }}}
 </head>
 <body>
 
