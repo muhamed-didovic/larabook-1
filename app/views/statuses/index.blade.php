10211ad0671cb2b0d3bfcc1d3cda406e226424f4
Ian Simmons <isimmons33@gmail.com>
2014-07-18 18:58:06 -0400

    styles and syntax

--- app/views/statuses/index.blade.php
+++ app/views/statuses/index.blade.php
@@ -1,26 +1,27 @@
 @extends('layouts.default')
 
 @section('content')
-    <h1>Post a status</h1>
+<div class="row">
+    <div class="col-md-6 col-md-offset-3">
 
-    @include('layouts.partials.form_errors')
+        @include('layouts.partials.form_errors')
 
-    {{{ Form::open(['route' => 'status.store']) }}}
-        <div class="form-group">
-            {{{ Form::label('Status', 'Status:') }}}
-            {{{ Form::textarea('body', null, ['class' => 'form-control']) }}}
+        <div class="status-post">
+            {{{ Form::open(['route' => 'status.store']) }}}
+                <div class="form-group">
+                    {{{ Form::label('Status', 'Status:', ['class' => 'sr-only sr-only-focusable']) }}}
+                    {{{ Form::textarea('body', null, ['rows' => 3, 'class' => 'form-control', 'placeholder' => 'What up fool!?']) }}}
+                </div>
+
+                <div class="form-group status-post-submit">
+                    {{{ Form::submit('Post Status', ['class' => 'btn btn-default btn-xs']) }}}
+                </div>
+            {{{ Form::close() }}}
         </div>
-
-        <div class="form-group">
-            {{{ Form::submit('Post Status', ['class' => 'btn btn-primary']) }}}
-        </div>
-    {{{ Form::close() }}}
-
-    <h2>Statuses</h2>
-    @foreach($statuses as $status)
-        <article>
-            {{ $status->body }}
-        </article>
-    @endforeach
+        @foreach($statuses as $status)
+            @include('statuses.partials.status')
+        @endforeach
+    </div>
+</div>
 @stop
 
