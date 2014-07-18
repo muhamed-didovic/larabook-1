e9111d31b83ee0d9163d9b577bcba42d03c37724
Ian Simmons <isimmons33@gmail.com>
2014-07-17 21:18:08 -0400

    playing with robo nothing here to see

---
+++ RoboFile.php
@@ -0,0 +1,30 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+
+use Robo\Tasks;
+
+class RoboFile extends Tasks {
+    public function sayHello()
+    {
+        // $codePage = exec('chcp');
+        // if($codePage != '65001')
+        // {
+        //     $this->say("sorry no utf-8 support for you. chcp is {$codePage}");
+        //     //output:  sorry no utf-8 support for you. chcp is Active code page: 437
+        //     $this->say("Making things right");
+        //     exec('chcp 65001');
+        //     $this->say('You should see an arrow now');
+        //     // output: ➜  You should see an arrow now
+        //     $this->yell('FOOBAR!');
+        //     /* output
+        //     ➜
+        //     ➜                 FOOBAR!
+        //     ➜
+        //     */
+        //     $this->ask('Now let me ask you a question');
+        //     //output: ?  Now let me ask you a question
+        // }
+
+        echo "\[1;30;40mλ";
+    }
+}
