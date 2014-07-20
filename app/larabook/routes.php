89d13dea7e3ea4a8d49ef9f9acf7c5c69f68e3cf
Ian Simmons <isimmons33@gmail.com>
2014-07-19 21:32:58 -0400

    move filters and routes into Larabook namespace

---
+++ app/larabook/routes.php
@@ -0,0 +1,71 @@
+<?php
+
+Route::get('/', [
+    'as' => 'home',
+    'uses' => 'Larabook\Controllers\PagesController@splash'
+]);
+
+
+/*
+|--------------------------------------------------------------------------
+| Session Routes
+|--------------------------------------------------------------------------
+*/
+/* login/logout */
+Route::get('login', [
+    'as' => 'login.create',
+    'before' => 'guest',
+    'uses' => 'Larabook\Controllers\SessionsController@create'
+]);
+
+Route::post('login', [
+    'as' => 'login.store',
+    'before' => 'csrf',
+    'uses' => 'Larabook\Controllers\SessionsController@store'
+]);
+
+Route::get('logout', [
+    'as' => 'logout',
+    'before' => 'auth',
+    'uses' => 'Larabook\Controllers\SessionsController@destroy'
+]);
+/* registration */
+Route::get('register', [
+    'as' => 'register.create',
+    'before' => 'guest',
+    'uses' => 'Larabook\Controllers\RegistrationController@create'
+]);
+
+Route::post('register', [
+    'as' => 'register.store',
+    'before' => 'csrf',
+    'uses' => 'Larabook\Controllers\RegistrationController@store'
+]);
+
+/*
+|--------------------------------------------------------------------------
+| Statuses
+|--------------------------------------------------------------------------
+*/
+Route::get('statuses', [
+    'as' => 'statuses',
+    'uses' => 'Larabook\Controllers\StatusesController@index'
+]);
+Route::post('statuses', [
+    'as' => 'status.store',
+    'before' => 'csrf|auth',
+    'uses' => 'Larabook\Controllers\StatusesController@store'
+]);
+
+/*
+|--------------------------------------------------------------------------
+| Settings
+|--------------------------------------------------------------------------
+*/
+Route::get('settings/{username}', [
+    'as' => 'user.settings',
+    'before' => 'auth',
+    'uses' => 'Larabook\Controllers\UsersController@settings'
+]);
+
+
