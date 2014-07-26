5e46f203dcb510e6f5ecd560c3a858a47a11783f
Ian Simmons <isimmons33@gmail.com>
2014-07-26 19:36:58 -0400

    make avatar clickable

--- app/views/layouts/partials/avatar.blade.php
+++ app/views/layouts/partials/avatar.blade.php
@@ -1,1 +1,3 @@
-<img class="media-object img-circle avatar" src="{{ $user->present()->gravatar(isset($size) ? $size : 30) }}" alt="{{ $user->username }}"></img>
+<a href="{{ route('users.profile', $user->username) }}">
+    <img class="media-object img-circle avatar" src="{{ $user->present()->gravatar(isset($size) ? $size : 30) }}" alt="{{ $user->username }}"></img>
+</a>
