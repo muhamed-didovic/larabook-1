6a1ee83452886939e61f02b185726d213628ad64
Ian Simmons <isimmons33@gmail.com>
2014-07-26 15:45:31 -0400

    seed users and statuses

---
+++ app/database/seeds/StatusesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Faker\Factory as Faker;
+use Larabook\Entities\Status\Status;
+use Larabook\Entities\User\User;
+
+class StatusesTableSeeder extends Seeder {
+
+    public function run()
+    {
+        $faker = Faker::create();
+
+        $userIds = User::lists('id');
+
+        foreach(range(1, 50) as $index)
+        {
+            Status::create([
+                'user_id' => $faker->randomElement($userIds),
+                'body' => $faker->sentence()
+            ]);
+        }
+    }
+
+}
---
+++ app/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Faker\Factory as Faker;
+use Larabook\Entities\User\User;
+
+class UsersTableSeeder extends Seeder {
+
+    public function run()
+    {
+        $faker = Faker::create();
+
+        //create one known user for manual login
+        User::create([
+            'username' => 'lotus',
+            'email' => 'lotus@gmail.com',
+            'password' => 'secret'
+        ]);
+
+        foreach(range(1, 10) as $index)
+        {
+            User::create([
+                'username' => $faker->word . $index,
+                'email' => $faker->email,
+                'password' => 'secret'
+            ]);
+        }
+    }
+
+}
