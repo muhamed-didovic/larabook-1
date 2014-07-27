dbcc0d02b8cf3aa4cf2cab27fb54ab5720409993
Ian Simmons <isimmons33@gmail.com>
2014-07-26 23:09:14 -0400

    refactor: use new forelse

--- app/views/statuses/partials/statuses.blade.php
+++ app/views/statuses/partials/statuses.blade.php
@@ -1,7 +1,5 @@
-@if($statuses->count())
-    @foreach($statuses as $status)
+@forelse($statuses as $status)
         @include('statuses.partials.status')
-    @endforeach
-@else
+@empty
     <p>This user is a 'Follow Whore'. They set up an account and follow to get follows but never post any statuses.</p>
-@endif
+@endforelse
