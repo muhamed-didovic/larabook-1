71949ff71408e79cba9271348f5ce25cc6dbd56a
Ian Simmons <isimmons33@gmail.com>
2014-07-26 23:23:03 -0400

    refactor: wrap form

--- app/views/sessions/create.blade.php
+++ app/views/sessions/create.blade.php
@@ -1,22 +1,26 @@
 @extends('layouts.default')
 
 @section('content')
-    <h1>Sign In!</h1>
+<h1>Sign In!</h1>
 
-    {{{ Form::open(['route' => 'login.store']) }}}
-        <div class="form-group">
-            {{{ Form::label('email', 'Email:') }}}
-            {{{ Form::email('email', null, ['class' => 'form-control', 'required' => 'required']) }}}
-        </div>
+<div class="row">
+    <div class="col-md-6">
+        {{{ Form::open(['route' => 'login.store']) }}}
+            <div class="form-group">
+                {{{ Form::label('email', 'Email:') }}}
+                {{{ Form::email('email', null, ['class' => 'form-control', 'required' => 'required']) }}}
+            </div>
 
-        <div class="form-group">
-            {{{ Form::label('password', 'Password:') }}}
-            {{{ Form::password('password', ['class' => 'form-control', 'required' => 'required']) }}}
-        </div>
+            <div class="form-group">
+                {{{ Form::label('password', 'Password:') }}}
+                {{{ Form::password('password', ['class' => 'form-control', 'required' => 'required']) }}}
+            </div>
 
-        <div class="form-group">
-            {{{ Form::submit('Sign In', ['class' => 'btn btn-primary']) }}}
-        </div>
+            <div class="form-group">
+                {{{ Form::submit('Sign In', ['class' => 'btn btn-primary']) }}}
+            </div>
 
-    {{{ Form::close() }}}
+        {{{ Form::close() }}}
+    </div>
+</div>
 @stop
