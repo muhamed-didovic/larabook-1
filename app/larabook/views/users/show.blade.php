7502a028fe8b75d2ae8a0a51c6e23667b736c9c7
Ian Simmons <isimmons33@gmail.com>
2014-08-09 19:21:51 -0400

    styling user

--- app/larabook/views/users/show.blade.php
+++ app/larabook/views/users/show.blade.php
@@ -2,13 +2,21 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-3">
-            <h1>{{ $user->username }}</h1>
-            @include('users.partials.avatar', ['size' => 100])
+        <div class="col-md-4">
+            <div class="media">
+                <div class="pull-left">
+                    @include('users.partials.avatar', ['size' => 65])
+                </div>
 
-            @unless($user->isCurrent($currentUser))
-                @include('users.partials.follow-form')
-            @endunless
+                <div class="media-body">
+                    <h1 class="media-heading">{{ $user->username }}</h1>
+                    <p class="text-muted">{{ $statusCount = $user->statuses->count() }} {{ str_plural('Status', $statusCount) }}</p>
+
+                    @unless($user->isCurrent($currentUser))
+                        @include('users.partials.follow-form')
+                    @endunless
+                </div>
+            </div>
         </div>
 
         <div class="col-md-6">
