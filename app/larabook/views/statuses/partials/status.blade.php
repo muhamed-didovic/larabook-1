613d89b7e663760162108d2be8ff4a5f466e1c1f
Ian Simmons <isimmons33@gmail.com>
2014-08-16 10:47:18 -0400

    add follow/unfollow partial

--- app/larabook/views/statuses/partials/status.blade.php
+++ app/larabook/views/statuses/partials/status.blade.php
@@ -6,5 +6,7 @@
         <h4 class="media-heading">{{ $status->user->username }}</h4>
         <p>{{ $status->present()->timeSincePublished }}</p>
         {{ $status->body }}
+
+        @include('users.partials.follow-form', ['user' => $status->user])
     </div>
 </article>
